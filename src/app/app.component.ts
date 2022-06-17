bb2e1c8545b5284a61c099bebd21ac8acf8f1f59
xv7ro <a.k_windsor@protonmail.com>
2022-06-17 21:15:10 +0200

    push all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  todos: Array<any>;
+
+  constructor(firestore: Firestore) {
+    const coll = collection(firestore, 'todo'); // collection from Firebase-data
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe((newTodos) => {
+      // Subsciribe to call the function
+      console.log('Neue Todos;', newTodos);
+      this.todos = newTodos;
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'akw-todolist',
+    appId: '1:229174237906:web:8d570d37f67ef982c6eb0b',
+    storageBucket: 'akw-todolist.appspot.com',
+    apiKey: 'AIzaSyC6cpHkEj6fu20IjqQl96VcbrVyGO3ml2c',
+    authDomain: 'akw-todolist.firebaseapp.com',
+    messagingSenderId: '229174237906',
+  },
   production: false
 };
 
